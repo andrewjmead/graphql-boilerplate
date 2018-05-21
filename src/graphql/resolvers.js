77384e4eea520a358d17c84a6780fa8225c47ac4
Andrew Mead <andrewjosephmead@gmail.com>
2018-05-21 09:49:12 -0400

    Setup example subscription

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,12 @@
 module.exports.resolvers = {
     Query: {
         hello: () => 'Hello!'
+    },
+    Subscription: {
+        count: {
+            subscribe(obj, args, { pubsub }) {
+                return pubsub.asyncIterator('COUNT')
+            }
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ const { GraphQLServer, PubSub } = require('graphql-yoga')
 const { resolvers } = require('./graphql/resolvers')
 const schema = fs.readFileSync(path.join(__dirname, './graphql/schema.gql')).toString()
 
+// Setup pub/sub for subscriptions
+const pubsub = new PubSub()
+
+// Demo subscription change
+let count = 0
+setInterval(() => {
+    pubsub.publish('COUNT', { count: count++ })
+}, 1000)
+
 // Define the GraphQL server
 const server = new GraphQLServer({
     typeDefs: schema,
-    resolvers
+    resolvers,
+    context: {
+        pubsub
+    }
 })
 
 server.start({
